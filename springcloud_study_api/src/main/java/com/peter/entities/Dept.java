debe64d9c02ca2d7f06f3e7e1a0857a8a9d78411
name <2819149088@qq.com>
2019-06-09 21:09:56 +0800

    1）代码的提交

--- springcloud_study_api/src/main/java/com/peter/entities/Dept.java
+++ springcloud_study_api/src/main/java/com/peter/entities/Dept.java
@@ -11,9 +11,9 @@ import java.io.Serializable;
  * @db_source;  //来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
  */
 public class Dept implements Serializable {
-    private Long  deptno;
-    private String  dname;
-    private String  db_source;
+    private Long  deptno; //主键
+    private String  dname; //名字
+    private String  db_source; //数据库资源
 
     public Dept() {
     }
