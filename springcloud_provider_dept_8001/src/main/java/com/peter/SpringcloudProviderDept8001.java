96e48cd1c5399b82919dad24039e8af46618fac5
name <2819149088@qq.com>
2019-06-11 07:43:16 +0800

    1）代码的提交

--- springcloud_provider_dept_8001/src/main/java/com/peter/SpringcloudProviderDept8001.java
+++ springcloud_provider_dept_8001/src/main/java/com/peter/SpringcloudProviderDept8001.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient        //本服务启动后会自动注册进eureka服务中
-public class SpringcloudStudyProviderDept8001 {
+public class SpringcloudProviderDept8001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudStudyProviderDept8001.class, args);
+        SpringApplication.run(SpringcloudProviderDept8001.class, args);
     }
 
 }
