b65821569db3aab2b062c11e2822e5f7690879c0
name <2819149088@qq.com>
2019-06-10 22:42:37 +0800

    1）提交代码

--- springcloud_eureka_7001/src/main/java/com/peter/SpringcloudEureka7001.java
+++ springcloud_eureka_7001/src/main/java/com/peter/SpringcloudEureka7001.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer    //EurekaServer服务器端启动类,接受其它微服务注册进来
+//@EnableEurekaServer    //EurekaServer服务器端启动类,接受其它微服务注册进来
 public class SpringcloudEureka7001 {
 
     public static void main(String[] args) {
