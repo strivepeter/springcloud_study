0e784800a45e58bacf8cc90740f04c77e8217581
name <2819149088@qq.com>
2019-06-10 08:45:17 +0800

    1）代码提交

---
+++ springcloud_consumer-dept-80/src/main/java/com/peter/SpringcloudConsumerDept80.java
@@ -0,0 +1,13 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudConsumerDept80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConsumerDept80.class, args);
+    }
+
+}
---
+++ springcloud_study_provider_dept_8001/src/main/java/com/peter/SpringcloudStudyProviderDept8001.java
@@ -0,0 +1,13 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStudyProviderDept8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudStudyProviderDept8001.class, args);
+    }
+
+}
