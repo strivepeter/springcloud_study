2765a3b1fa5f97fe8b6900559b1a9f2d4dfdb1a9
name <2819149088@qq.com>
2019-06-10 21:37:52 +0800

    1）添加Eureka

--- springcloud_study_provider_dept_8001/src/main/java/com/peter/SpringcloudStudyProviderDept8001.java
+++ springcloud_study_provider_dept_8001/src/main/java/com/peter/SpringcloudStudyProviderDept8001.java
@@ -2,8 +2,10 @@ package com.peter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient        //本服务启动后会自动注册进eureka服务中
 public class SpringcloudStudyProviderDept8001 {
 
     public static void main(String[] args) {
