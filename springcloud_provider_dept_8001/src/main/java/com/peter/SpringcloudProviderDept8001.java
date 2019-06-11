1d29b8cc3baa96fadae8d45a2c778a5502af3a7a
name <2819149088@qq.com>
2019-06-11 21:20:29 +0800

    1）完善单列的服务注册与发现

--- springcloud_provider_dept_8001/src/main/java/com/peter/SpringcloudProviderDept8001.java
+++ springcloud_provider_dept_8001/src/main/java/com/peter/SpringcloudProviderDept8001.java
@@ -2,10 +2,12 @@ package com.peter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient        //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient     //服务发现者
 public class SpringcloudProviderDept8001 {
 
     public static void main(String[] args) {
