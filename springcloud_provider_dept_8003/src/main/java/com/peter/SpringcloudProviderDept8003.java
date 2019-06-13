a62871fbe7d58281720984bf3b059126497f6e24
name <2819149088@qq.com>
2019-06-13 09:36:12 +0800

    1)提交代码到仓库

---
+++ springcloud_provider_dept_8003/src/main/java/com/peter/SpringcloudProviderDept8003.java
@@ -0,0 +1,15 @@
+package com.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient        //本服务启动后会自动注册进eureka服务中
+public class SpringcloudProviderDept8003 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProviderDept8003.class, args);
+    }
+
+}
